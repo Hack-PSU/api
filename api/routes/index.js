cf48d7af4427a5b5cbb43cec695b9c70a48c5a73
Sushrut Shringarputale <contact@sushshring.me>
2018-01-30 21:06:32 -0500

    Added current status of server

---
+++ api/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  res.status(200).set('content-type', 'application/json').send({ response: 'Welcome to HackPSU 2018. Make sure you have your credentials!' });
+});
+
+module.exports = router;

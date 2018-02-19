0b708733e23daf19ada1ee6fd5e4e9eb9749987e
unknown <rbo5046@psu.edu>
2018-02-19 15:35:10 -0500

    added registration api form validation, and resume upload

--- api/helpers/constants.js
+++ api/helpers/constants.js
@@ -23,5 +23,131 @@ module.exports = {
         user     : process.env.RDS_USERNAME || 'user',
         password : process.env.RDS_PASSWORD || 'secret',
         database : process.env.RDS_DATABASE || 'my_db',
-    }
-};
+    },
+    registeredUserSchema: {
+        type:'object',
+        properties: {
+            firstName: {
+                type: 'string',
+                minLength: 1,
+                maxLength: 45,
+            },
+            lastName: {
+                type: 'string',
+                minLength: 1,
+                maxLength: 45,
+            },
+            gender: { 
+                "enum":  ['Male','Female','Non-Binary','Prefer not to disclose']
+            },
+            shirtSize:{ 
+                "enum": ['XS','S','M','L','XL','XXL']
+            },
+            dietaryRestriction: {
+                "enum": ['Vegetarian','Vegan','Kosher','Allergies']  
+            },
+            allergies: {
+                type: 'string',
+                minLength: 1,
+                maxLength: 45,
+            },
+            travelReimbursement: {
+                type: 'boolean'
+            }, 
+            firstHackathon: {
+                type: 'boolean'
+            },
+            university: {
+                type: 'string',
+                minLength: 1, 
+                maxLength: 200,
+            },
+            email: {
+                type: 'string',
+                format: 'email'   
+            },
+            academicYear: {
+                "enum": ["Freshman", "Sophomore", "Junior", "Senior", "Graduate student", "Graduated within last 12 months"]
+            },
+            major: {
+                type: 'string',
+                minLength: 1,
+                maxLength: 100
+            },
+            phone: {
+                type: 'string', 
+                minLength: 1,
+                maxLength: 50
+            },
+            race: {
+                type: 'string',
+                maxLength: 150
+            },
+            codingExperience: {
+                "enum": ["None","Beginner", "Intermediate", "Advanced"]
+            },
+            uid: {
+                type: 'string',
+                maxLength: 150
+            },
+            eighteenBeforeEvent: {
+                type: 'boolean'
+            }, 
+            mlhCOC: {
+                type: 'boolean' 
+            },
+            mlhDCP: {
+                type: 'boolean'
+            },
+            referral: {
+                type: 'string'
+            },
+            project: { 
+                type: 'string'
+            }
+
+
+            },
+            required: ['firstName', 'lastName', 'gender', 'shirtSize', 'travelReimbursement', 'firstHackathon', 'email', 'academicYear', 'major', 'phone', 'codingExperience', 'uid', 'eighteenBeforeEvent', 'mlhCOC', 'mlhDCP']
+
+        }
+    };
+
+//     First name
+// Last name
+// Gender
+// - Male
+// - Female
+// - Non-Binary
+// - Prefer not to disclose
+// Will you be 18 before the date (ILLEGAL TO SAY NO, CANCEL REGISTRATION)
+// Shirt size
+// - XS, S, M, L, XL, XXL
+// Dietary restriction
+// - Vegetarian
+// - Vegan
+// - Kosher
+// - Allergies
+// -- Jump to describe
+// Will you need travel reimbursement?
+// Is this your first hackathon?
+// Name of University
+// - Type in on other
+// School email
+// Academic Year
+// (Intended) Major
+// Resume
+// Agree to MLH COC?
+
+// -----------------------------------------------
+// *Demographics*
+// Race/Ethnicities?
+// Languages spoken?
+// Sexual orientation?
+// Coding experience?
+// __________________________________________________________
+// *New questions*
+// Where did you hear about hackPSU?
+// What is a project you're proud of?
+
+

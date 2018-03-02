e285676288922d93f2dd0cb421c7b14bec86b02d
Suilong Luo <suilong24@yahoo.com>
2018-03-01 20:28:08 -0500

    code clean up fixed error from previous commit

--- api/helpers/constants.js
+++ api/helpers/constants.js
@@ -17,6 +17,10 @@ module.exports = {
         },
         required: ['email', 'name'],
     },
+    emailSchema:{
+      type: 'string',
+      format: 'email'
+    },
    emailKey: {
         key: process.env.AWS_ACCESS_KEY_ID,
         secret: process.env.AWS_SECRET_ACCESS_KEY
--- api/helpers/functions.js
+++ api/helpers/functions.js
@@ -11,24 +11,24 @@ const client = ses.createClient(emailKey);
  * @param {String} html A string of HTML text that forms the body of the email. All substitutables must be formatted as $substitutable$. The HTML MUST contain the $NAME$ substitutable.
  * @param {String} name The name of the recipient
  * @param {Object} [substitutions] A map of strings with the following format { keyword-to-substitute: string-to-substitute-with, ... }; Example: { date: "09-23-2000" }
- * @return {String} HTML string with the words properly substituted
+ * @return {Promise} return a promised data with a subbed version of the html
  */
 module.exports.emailSubstitute = function emailSubstitute(html, name, substitutions) {
   let promise = new Promise(function(resolve, reject) {
     let subbedHTML = html.replace(/\$name\$/g, name);
     Object.entries(substitutions).forEach((substitution) => {
-      if (substitution[1].length > 0 && substitution[0].length > 0){
+      if (substitution[1].length > 0 && substitution[0].length > 0){  
         console.log(substitution[0].length,substitution[0],substitution[1]);
         subbedHTML = subbedHTML.replace(new RegExp(`\\$${substitution[0]}\\$`, 'g'), substitution[1]);
-        resolve(subbedHTML);
       }
       else{
         console.log("failed");
         const error = new Error();
-        error.body = {error: 'One or more substitution field or tag is empty'};
+        error.body = {error: 'One or more substitution keyword or substitution-text is empty'};
         reject(error)
       }
     });
+    resolve(subbedHTML)
   })
   return promise;
 };
@@ -67,15 +67,13 @@ module.exports.sendEmail = function sendEmail(data) {
  * @return {Object} { data, options }
  */
 module.exports.createEmailRequest = function createEmailRequest(email, htmlContent, subject, fromEmail) {
-  const validate = ajv.compile(constants.emailObjectSchema);
+  const validate = ajv.compile(constants.emailSchema);
   let emailAddress;
   console.log(fromEmail);
-  if (validate(fromEmail)) {   //not working
-    console.log(fromEmail);
+  if (validate(fromEmail)) {
     emailAddress = fromEmail;
   }
   else{
-    console.log("validate email failed");
     emailAddress = 'team@hackpsu.org';
   }
   const data = {

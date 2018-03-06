4924ac6658e72e03e67b82f145986f69ce5bdbeb
Sushrut Shringarputale <contact@sushshring.me>
2018-03-06 09:50:56 -0500

    - Minor syntactical errors and removal of console.logs()

--- api/helpers/constants.js
+++ api/helpers/constants.js
@@ -17,10 +17,6 @@ module.exports = {
         },
         required: ['email', 'name'],
     },
-    emailSchema:{
-      type: 'string',
-      format: 'email'
-    },
    emailKey: {
         key: process.env.AWS_ACCESS_KEY_ID,
         secret: process.env.AWS_SECRET_ACCESS_KEY

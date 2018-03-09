267ab9b3365c7d3ba25a97ca8d6b4295c837b3ab
Sushrut Shringarputale <contact@sushshring.me>
2018-03-09 12:41:39 -0500

    - Added ESLint for code quality. Follow the Airbnb standard - Added socket for live-updates. It should be working quite nicely now

--- api/helpers/database.js
+++ api/helpers/database.js
@@ -1,14 +1,16 @@
 const squel = require('squel');
 const sql = require('mysql');
 const uuidv4 = require('uuid/v4');
+const timeuuid = require('node-time-uuid');
 
 const sqlOptions = require('../helpers/constants').sqlConnection;
+
 const connection = sql.createConnection(sqlOptions);
 
 connection.connect((err) => {
-    if (err) {
-        console.error(err.stack);
-    }
+  if (err) {
+    console.error(err.stack);
+  }
 });
 
 /**
@@ -17,15 +19,15 @@ connection.connect((err) => {
  * @return {Stream} Returns a continuous stream of data from the database
  */
 function getRegistrations(limit, offset) {
-    const mLimit = parseInt(limit);
-    const mOffset = parseInt(offset);
-    const query = squel.select({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .from(process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION')
-        .limit(mLimit ? mLimit : null)
-        .offset(mOffset ? mOffset : null)
-        .toString()
-        .concat(';');
-    return connection.query(query).stream();
+  const mLimit = parseInt(limit);
+  const mOffset = parseInt(offset);
+  const query = squel.select({ autoQuoteTableNames: true, autoQuoteFieldNames: true })
+    .from(process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION')
+    .limit(mLimit || null)
+    .offset(mOffset || null)
+    .toString()
+    .concat(';');
+  return connection.query(query).stream();
 }
 
 /**
@@ -34,12 +36,12 @@ function getRegistrations(limit, offset) {
  * @return {Stream} Returns a continuous stream of data from the database
  */
 function getRegistration(uid) {
-    const query = squel.select({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .from(process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION')
-        .where('uid = ?', uid)
-        .toParam();
-    query.text = query.text.concat(';');
-    return connection.query(query.text, query.values).stream();
+  const query = squel.select({ autoQuoteTableNames: true, autoQuoteFieldNames: true })
+    .from(process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION')
+    .where('uid = ?', uid)
+    .toParam();
+  query.text = query.text.concat(';');
+  return connection.query(query.text, query.values).stream();
 }
 
 /**
@@ -48,14 +50,15 @@ function getRegistration(uid) {
  * @return {Stream} Returns a continuous stream of data from the database
  */
 function getPreRegistrations(limit, offset) {
-    const mLimit = parseInt(limit);
-    const mOffset = parseInt(offset);
-    const query = squel.select({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .from('PRE_REGISTRATION')
-        .limit(mLimit ? limit : null)
-        .offset(mOffset ? offset : null)
-        .toString().concat(';');
-    return connection.query(query).stream();
+  const mLimit = parseInt(limit, 10);
+  const mOffset = parseInt(offset, 10);
+  const query = squel.select({ autoQuoteTableNames: true, autoQuoteFieldNames: true })
+    .from('PRE_REGISTRATION')
+    .limit(mLimit ? limit : null)
+    .offset(mOffset ? offset : null)
+    .toString()
+    .concat(';');
+  return connection.query(query).stream();
 }
 
 /**
@@ -64,27 +67,27 @@ function getPreRegistrations(limit, offset) {
  * @return {Promise<any>}
  */
 function addPreRegistration(email) {
-    const query = squel.insert({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .into(process.env.NODE_ENV === 'test' ? 'PRE_REGISTRATION_TEST' : 'PRE_REGISTRATION')
-        .setFieldsRows([
-            {id: uuidv4().replace(/-/g, ""), email: email},
-        ])
-        .toParam();
-    query.text = query.text.concat(';');
-    return new Promise((resolve, reject) => {
-        connection.query(query.text, query.values, (err, result) => {
-            if (err && err.errno === 1062) {
-                const error = new Error();
-                error.message = "Email already provided";
-                error.status = 400;
-                reject(error);
-            } else if (err) {
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        });
+  const query = squel.insert({ autoQuoteTableNames: true, autoQuoteFieldNames: true })
+    .into(process.env.NODE_ENV === 'test' ? 'PRE_REGISTRATION_TEST' : 'PRE_REGISTRATION')
+    .setFieldsRows([
+      { id: uuidv4().replace(/-/g, ''), email },
+    ])
+    .toParam();
+  query.text = query.text.concat(';');
+  return new Promise((resolve, reject) => {
+    connection.query(query.text, query.values, (err, result) => {
+      if (err && err.errno === 1062) {
+        const error = new Error();
+        error.message = 'Email already provided';
+        error.status = 400;
+        reject(error);
+      } else if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
     });
+  });
 }
 
 
@@ -93,14 +96,14 @@ function addPreRegistration(email) {
  * @param uid {string} UID of the user to set registration submission status
  */
 function setRegistrationSubmitted(uid) {
-    const dbname = process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION';
-    const query = squel.update({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .table(dbname)
-        .set('submitted', true)
-        .where("uid = ?", uid)
-        .toParam();
-    query.text = query.text.concat(';');
-    connection.query(query.text, query.values);
+  const dbname = process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION';
+  const query = squel.update({ autoQuoteTableNames: true, autoQuoteFieldNames: true })
+    .table(dbname)
+    .set('submitted', true)
+    .where('uid = ?', uid)
+    .toParam();
+  query.text = query.text.concat(';');
+  connection.query(query.text, query.values);
 }
 
 
@@ -110,26 +113,26 @@ function setRegistrationSubmitted(uid) {
  * @return {Promise<any>}
  */
 function addRegistration(data) {
-    const query = squel.insert({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .into(process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION')
-        .setFieldsRows([
-            data
-        ]).toParam();
-    query.text = query.text.concat(';');
-    return new Promise((resolve, reject) => {
-        connection.query(query.text, query.values, (err, result) => {
-            if (err && err.errno === 1062) {
-                const error = new Error();
-                error.message = "User already registered";
-                error.status = 400;
-                reject(error);
-            } else if (err) {
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        });
+  const query = squel.insert({ autoQuoteTableNames: true, autoQuoteFieldNames: true })
+    .into(process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION')
+    .setFieldsRows([
+      data,
+    ]).toParam();
+  query.text = query.text.concat(';');
+  return new Promise((resolve, reject) => {
+    connection.query(query.text, query.values, (err, result) => {
+      if (err && err.errno === 1062) {
+        const error = new Error();
+        error.message = 'User already registered';
+        error.status = 400;
+        reject(error);
+      } else if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
     });
+  });
 }
 
 /**
@@ -137,53 +140,102 @@ function addRegistration(data) {
  * @param msg {String} Message to write
  */
 function writePiMessage(msg) {
-    const query = squel.insert({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .into('PI_TEST')
-        .setFieldsRows([
-            {time: new Date().getTime(), message: msg}
-        ])
-        .toParam();
-    query.text = query.text.concat(';');
-    return new Promise((resolve, reject) => {
-        connection.query(query.text, query.values, (err) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(msg);
-            }
-        });
+  const query = squel.insert({ autoQuoteTableNames: true, autoQuoteFieldNames: true })
+    .into('PI_TEST')
+    .setFieldsRows([
+      { time: new Date().getTime(), message: msg },
+    ])
+    .toParam();
+  query.text = query.text.concat(';');
+  return new Promise((resolve, reject) => {
+    connection.query(query.text, query.values, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(msg);
+      }
     });
+  });
 }
 
 /**
  *
  * @param ipAddress {string} The IP Address to store
- * @param user_agent {string} The user-agent field
+ * @param userAgent {string} The user-agent field
  * @return Promise<any> {Promise<any>}
  */
-function storeIP(ipAddress, user_agent) {
-    const query = squel.insert({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .into('REQ_DATA')
-        .setFieldsRows([
-            {idREQ_DATA: uuidv4(), req_time: new Date().getTime(), req_ip: ipAddress ? ipAddress : "", req_user_agent: user_agent}
-        ])
-        .toParam();
-    query.text = query.text.concat(';');
-    return new Promise((resolve) => {
-        connection.query(query.text, query.values, () => {
-            resolve();
-        });
+function storeIP(ipAddress, userAgent) {
+  const query = squel.insert({ autoQuoteTableNames: true, autoQuoteFieldNames: true })
+    .into('REQ_DATA')
+    .setFieldsRows([
+      {
+        idREQ_DATA: uuidv4(), req_time: new Date().getTime(), req_ip: ipAddress || '', req_user_agent: userAgent,
+      },
+    ])
+    .toParam();
+  query.text = query.text.concat(';');
+  return new Promise((resolve) => {
+    connection.query(query.text, query.values, () => {
+      resolve();
     });
+  });
+}
+
+/**
+ * @return {Promise} Returns all the data from the database
+ */
+function getCurrentUpdates() {
+  let query = squel.select({ autoQuoteTableNames: true, autoQuoteFieldNames: true })
+    .from('LIVE_UPDATES')
+    .order('idLIVE_UPDATES')
+    .toString();
+  query = query.concat(';');
+  return new Promise((resolve, reject) => {
+    connection.query(query, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+/**
+ *
+ * @param updateText {string}
+ * @param updateImage {string}
+ * @return {Promise<any>}
+ */
+function addNewUpdate(updateText, updateImage) {
+  const insertObj = {
+    idLIVE_UPDATES: new timeuuid().toString('hex'), update_text: updateText, update_image: updateImage,
+  };
+  const query = squel.insert({ autoQuoteTableNames: true, autoQuoteFieldNames: true })
+    .into('LIVE_UPDATES')
+    .setFieldsRows([insertObj]).toParam();
+  query.text = query.text.concat(';');
+  return new Promise((resolve, reject) => {
+    connection.query(query.text, query.values, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(insertObj);
+      }
+    });
+  });
 }
 
 
 module.exports = {
-    getRegistrations,
-    getRegistration,
-    getPreRegistrations,
-    addPreRegistration,
-    writePiMessage,
-    addRegistration,
-    setRegistrationSubmitted,
-    storeIP,
+  getRegistrations,
+  getRegistration,
+  getPreRegistrations,
+  addPreRegistration,
+  writePiMessage,
+  addRegistration,
+  setRegistrationSubmitted,
+  storeIP,
+  getCurrentUpdates,
+  addNewUpdate,
 };

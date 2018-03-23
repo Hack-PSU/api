2e3a1ff0e8cc7579f48ca41d69ed8772f495afc2
Sushrut Shringarputale <contact@sushshring.me>
2018-03-22 20:14:02 -0400

    Fixed compile and logic issues

--- api/helpers/database.js
+++ api/helpers/database.js
@@ -6,9 +6,9 @@ const sqlOptions = require('../helpers/constants').sqlConnection;
 const connection = sql.createConnection(sqlOptions);
 
 connection.connect((err) => {
-    if (err) {
-        console.error(err.stack);
-    }
+  if (err) {
+    console.error(err.stack);
+  }
 });
 
 /**
@@ -17,15 +17,15 @@ connection.connect((err) => {
  * @return {Stream} Returns a continuous stream of data from the database
  */
 function getRegistrations(limit, offset) {
-    const mLimit = parseInt(limit);
-    const mOffset = parseInt(offset);
-    const query = squel.select({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .from(process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION')
-        .limit(mLimit ? mLimit : null)
-        .offset(mOffset ? mOffset : null)
-        .toString()
-        .concat(';');
-    return connection.query(query).stream();
+  const mLimit = parseInt(limit);
+  const mOffset = parseInt(offset);
+  const query = squel.select({autoQuoteTableNames: true, autoQuoteFieldNames: true})
+    .from(process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION')
+    .limit(mLimit ? mLimit : null)
+    .offset(mOffset ? mOffset : null)
+    .toString()
+    .concat(';');
+  return connection.query(query).stream();
 }
 
 /**
@@ -34,12 +34,12 @@ function getRegistrations(limit, offset) {
  * @return {Stream} Returns a continuous stream of data from the database
  */
 function getRegistration(uid) {
-    const query = squel.select({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .from(process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION')
-        .where('uid = ?', uid)
-        .toParam();
-    query.text = query.text.concat(';');
-    return connection.query(query.text, query.values).stream();
+  const query = squel.select({autoQuoteTableNames: true, autoQuoteFieldNames: true})
+    .from(process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION')
+    .where('uid = ?', uid)
+    .toParam();
+  query.text = query.text.concat(';');
+  return connection.query(query.text, query.values).stream();
 }
 
 /**
@@ -48,14 +48,14 @@ function getRegistration(uid) {
  * @return {Stream} Returns a continuous stream of data from the database
  */
 function getPreRegistrations(limit, offset) {
-    const mLimit = parseInt(limit);
-    const mOffset = parseInt(offset);
-    const query = squel.select({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .from('PRE_REGISTRATION')
-        .limit(mLimit ? limit : null)
-        .offset(mOffset ? offset : null)
-        .toString().concat(';');
-    return connection.query(query).stream();
+  const mLimit = parseInt(limit);
+  const mOffset = parseInt(offset);
+  const query = squel.select({autoQuoteTableNames: true, autoQuoteFieldNames: true})
+    .from('PRE_REGISTRATION')
+    .limit(mLimit ? limit : null)
+    .offset(mOffset ? offset : null)
+    .toString().concat(';');
+  return connection.query(query).stream();
 }
 
 /**
@@ -64,27 +64,27 @@ function getPreRegistrations(limit, offset) {
  * @return {Promise<any>}
  */
 function addPreRegistration(email) {
-    const query = squel.insert({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .into(process.env.NODE_ENV === 'test' ? 'PRE_REGISTRATION_TEST' : 'PRE_REGISTRATION')
-        .setFieldsRows([
-            {id: uuidv4().replace(/-/g, ""), email: email},
-        ])
-        .toParam();
-    query.text = query.text.concat(';');
-    return new Promise((resolve, reject) => {
-        connection.query(query.text, query.values, (err, result) => {
-            if (err && err.errno === 1062) {
-                const error = new Error();
-                error.message = "Email already provided";
-                error.status = 400;
-                reject(error);
-            } else if (err) {
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        });
+  const query = squel.insert({autoQuoteTableNames: true, autoQuoteFieldNames: true})
+    .into(process.env.NODE_ENV === 'test' ? 'PRE_REGISTRATION_TEST' : 'PRE_REGISTRATION')
+    .setFieldsRows([
+      {id: uuidv4().replace(/-/g, ""), email: email},
+    ])
+    .toParam();
+  query.text = query.text.concat(';');
+  return new Promise((resolve, reject) => {
+    connection.query(query.text, query.values, (err, result) => {
+      if (err && err.errno === 1062) {
+        const error = new Error();
+        error.message = "Email already provided";
+        error.status = 400;
+        reject(error);
+      } else if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
     });
+  });
 }
 
 
@@ -93,14 +93,14 @@ function addPreRegistration(email) {
  * @param uid {string} UID of the user to set registration submission status
  */
 function setRegistrationSubmitted(uid) {
-    const dbname = process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION';
-    const query = squel.update({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .table(dbname)
-        .set('submitted', true)
-        .where("uid = ?", uid)
-        .toParam();
-    query.text = query.text.concat(';');
-    connection.query(query.text, query.values);
+  const dbname = process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION';
+  const query = squel.update({autoQuoteTableNames: true, autoQuoteFieldNames: true})
+    .table(dbname)
+    .set('submitted', true)
+    .where("uid = ?", uid)
+    .toParam();
+  query.text = query.text.concat(';');
+  connection.query(query.text, query.values);
 }
 
 
@@ -110,73 +110,71 @@ function setRegistrationSubmitted(uid) {
  * @return {Promise<any>}
  */
 function addRegistration(data) {
-    const query = squel.insert({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .into(process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION')
-        .setFieldsRows([
-            data
-        ]).toParam();
-    query.text = query.text.concat(';');
-    return new Promise((resolve, reject) => {
-        connection.query(query.text, query.values, (err, result) => {
-            if (err && err.errno === 1062) {
-                const error = new Error();
-                error.message = "User already registered";
-                error.status = 400;
-                reject(error);
-            } else if (err) {
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        });
+  const query = squel.insert({autoQuoteTableNames: true, autoQuoteFieldNames: true})
+    .into(process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION')
+    .setFieldsRows([
+      data
+    ]).toParam();
+  query.text = query.text.concat(';');
+  return new Promise((resolve, reject) => {
+    connection.query(query.text, query.values, (err, result) => {
+      if (err && err.errno === 1062) {
+        const error = new Error();
+        error.message = "User already registered";
+        error.status = 400;
+        reject(error);
+      } else if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
     });
+  });
 }
 
 /**
-*
-* @param uid {string} UID of the user to set the RSVP status
-*
-*/
-function setRSVP(uid, RSVPstatus){
-	const dbname = process.env.NODE_ENV === 'test' ? 'RSVP_TEST' : 'RSVP';
-	const query = squel.insert({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-		.into(dbname)
-		.setFieldRows([isRSVP: uid, rsvp_time: newDate().getTime(), rsvp_status: RSVPstatus])
-		.toParam();
-	query.text = query.text,concat(';');
-	return new Promise((resolve, reject) => {
-		connection.query(query.text, query.values, (err) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve();
-			}
-		});
-	});
-}
-	
-/**
-*
-* @param uid {string} UID of the user to get the RSVP status
-*
-**/
-//fix
-function getRSVP(uid){
-	const dbname = process.env.NODE_ENV === 'test' ? 'RSVP_TEST' : 'RSVP';
-	const query = squel.select({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-		.from(dbname)
-		.field('isRSVP')
-		.where("idRSVP = ?", uid)
-		.toString()
-		.concat(';');
+ *
+ * @param uid {string} UID of the user to set the RSVP status
+ * @param RSVPstatus
+ */
+function setRSVP(uid, RSVPstatus) {
+  const dbname = process.env.NODE_ENV === 'test' ? 'RSVP_TEST' : 'RSVP';
+  const query = squel.insert({autoQuoteTableNames: true, autoQuoteFieldNames: true})
+    .into(dbname)
+    .setFieldRows([{isRSVP: uid, rsvp_time: newDate().getTime(), rsvp_status: RSVPstatus}])
+    .toParam();
+  query.text = query.text.concat(';');
   return new Promise((resolve, reject) => {
-  	connection.query(query, (err, response) => {
-  		if (err) {
-  			reject(err);
-  		} else {
-  			resolve(response);
-  		}
-  	})
+    connection.query(query.text, query.values, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+/**
+ *
+ * @param uid {string} UID of the user to get the RSVP status
+ * @return {Promise<any>}
+ */
+function getRSVP(uid) {
+  const dbname = process.env.NODE_ENV === 'test' ? 'RSVP_TEST' : 'RSVP';
+  const query = squel.select({autoQuoteTableNames: true, autoQuoteFieldNames: true})
+    .from(dbname)
+    .where("idRSVP = ?", uid)
+    .toParam();
+  query.text = query.text.concat(';');
+  return new Promise((resolve, reject) => {
+    connection.query(query.text, query.values, (err, response) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(response);
+      }
+    })
   })
 }
 
@@ -184,24 +182,24 @@ function getRSVP(uid){
  *
  * @param uid {string} get email associated with the uid
  *
-**/
-function getEmail(uid){
-	const dbname = process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION';
-	const query = squel.select({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-		.from(dbname)
-		.field('email')
-		.where("uid = ?",uid)
-		.toString()
-		.concat(';');
-	return new Promise((resolve, reject) => {
-		connection.query(query, (err, reponse) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(response);
-			}
-		})
-	})
+ **/
+function getEmail(uid) {
+  const dbname = process.env.NODE_ENV === 'test' ? 'REGISTRATION_TEST' : 'REGISTRATION';
+  const query = squel.select({autoQuoteTableNames: true, autoQuoteFieldNames: true})
+    .from(dbname)
+    .field('email')
+    .where("uid = ?", uid)
+    .toString()
+    .concat(';');
+  return new Promise((resolve, reject) => {
+    connection.query(query, (err, response) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(response);
+      }
+    })
+  })
 }
 
 /**
@@ -209,22 +207,22 @@ function getEmail(uid){
  * @param msg {String} Message to write
  */
 function writePiMessage(msg) {
-    const query = squel.insert({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .into('PI_TEST')
-        .setFieldsRows([
-            {time: new Date().getTime(), message: msg}
-        ])
-        .toParam();
-    query.text = query.text.concat(';');
-    return new Promise((resolve, reject) => {
-        connection.query(query.text, query.values, (err) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(msg);
-            }
-        });
+  const query = squel.insert({autoQuoteTableNames: true, autoQuoteFieldNames: true})
+    .into('PI_TEST')
+    .setFieldsRows([
+      {time: new Date().getTime(), message: msg}
+    ])
+    .toParam();
+  query.text = query.text.concat(';');
+  return new Promise((resolve, reject) => {
+    connection.query(query.text, query.values, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(msg);
+      }
     });
+  });
 }
 
 /**
@@ -234,28 +232,36 @@ function writePiMessage(msg) {
  * @return Promise<any> {Promise<any>}
  */
 function storeIP(ipAddress, user_agent) {
-    const query = squel.insert({autoQuoteTableNames: true, autoQuoteFieldNames: true})
-        .into('REQ_DATA')
-        .setFieldsRows([
-            {idREQ_DATA: uuidv4(), req_time: new Date().getTime(), req_ip: ipAddress ? ipAddress : "", req_user_agent: user_agent}
-        ])
-        .toParam();
-    query.text = query.text.concat(';');
-    return new Promise((resolve) => {
-        connection.query(query.text, query.values, () => {
-            resolve();
-        });
+  const query = squel.insert({autoQuoteTableNames: true, autoQuoteFieldNames: true})
+    .into('REQ_DATA')
+    .setFieldsRows([
+      {
+        idREQ_DATA: uuidv4(),
+        req_time: new Date().getTime(),
+        req_ip: ipAddress ? ipAddress : "",
+        req_user_agent: user_agent
+      }
+    ])
+    .toParam();
+  query.text = query.text.concat(';');
+  return new Promise((resolve) => {
+    connection.query(query.text, query.values, () => {
+      resolve();
     });
+  });
 }
 
 
 module.exports = {
-    getRegistrations,
-    getRegistration,
-    getPreRegistrations,
-    addPreRegistration,
-    writePiMessage,
-    addRegistration,
-    setRegistrationSubmitted,
-    storeIP,
+  getRegistrations,
+  getRegistration,
+  getPreRegistrations,
+  addPreRegistration,
+  writePiMessage,
+  addRegistration,
+  setRegistrationSubmitted,
+  storeIP,
+  getRSVP,
+  setRSVP,
+  getEmail,
 };
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const functions = require("../helpers/functions");
 const authenticator = require("../helpers/auth");
 const constants = require("../helpers/constant");
 const database = require('../helpers/database');
@@ -10,23 +11,23 @@ const router = express.Router();
  * User authentication middleware
  */
 router.use((req, res, next) => {
-    if (req.headers.idtoken) {
-        authenticator.checkAuthentication(req.headers.idtoken)
-            .then((decodedToken) => {
-                res.locals.user = decodedToken;
-                next();
-            }).catch((err) => {
-            const error = new Error();
-            error.status = 401;
-            error.body = err.message;
-            next(error);
-        });
-    } else {
-        const error = new Error();
-        error.status = 401;
-        error.body = {error: 'ID Token must be provided'};
-        next(error);
-    }
+  if (req.headers.idtoken) {
+    authenticator.checkAuthentication(req.headers.idtoken)
+      .then((decodedToken) => {
+        res.locals.user = decodedToken;
+        next();
+      }).catch((err) => {
+      const error = new Error();
+      error.status = 401;
+      error.body = err.message;
+      next(error);
+    });
+  } else {
+    const error = new Error();
+    error.status = 401;
+    error.body = {error: 'ID Token must be provided'};
+    next(error);
+  }
 });
 
 
@@ -43,14 +44,14 @@ router.use((req, res, next) => {
  * @apiSuccess {Object} JSON Object with user's data
  */
 router.get('/', (req, res, next) => {
-    if (res.locals.user) {
-        res.status(200).send({admin: res.locals.user.admin, privilege: res.locals.user.privilege});
-    } else {
-        const error = new Error();
-        error.status = 500;
-        error.body = {error: 'Could not retrieve user information'};
-        next(error);
-    }
+  if (res.locals.user) {
+    res.status(200).send({admin: res.locals.user.admin, privilege: res.locals.user.privilege});
+  } else {
+    const error = new Error();
+    error.status = 500;
+    error.body = {error: 'Could not retrieve user information'};
+    next(error);
+  }
 });
 
 /**
@@ -65,89 +66,103 @@ router.get('/', (req, res, next) => {
  * @apiSuccess {Object} JSON Object with user's data
  */
 router.get('/registration', (req, res, next) => {
-   if (res.locals.user) {
-       let user = null;
-       database.getRegistration(res.locals.user.uid)
-           .on('data', (data) => {
-               user = data;
-           }).on('err', (err) => {
-           const error = new Error();
-           error.status = 500;
-           error.body = err.message;
-           next(error);
-       }).on('end', () => {
-          res.status(200).send(user);
-       });
-   } else {
-       const error = new Error();
-       error.status = 500;
-       error.body = {error: 'Could not retrieve user information'};
-       next(error);
-   }
+  if (res.locals.user) {
+    let user = null;
+    database.getRegistration(res.locals.user.uid)
+      .on('data', (data) => {
+        user = data;
+      }).on('err', (err) => {
+      const error = new Error();
+      error.status = 500;
+      error.body = err.message;
+      next(error);
+    }).on('end', () => {
+      res.status(200).send(user);
+    });
+  } else {
+    const error = new Error();
+    error.status = 500;
+    error.body = {error: 'Could not retrieve user information'};
+    next(error);
+  }
 });
 
 
 /**
- * @api {get} /users/RSVP confirm the RSVP status for the current user and send a email containing their pin
- * @apiVersion 0.1.1
- * @apiName RSVP
+ * @api {post} /users/RSVP confirm the RSVP status for the current user and send a email containing their pin
+ * @apiVersion 0.3.1
+ * @apiName Set RSVP
  *
  * @apiGroup Users
  * @apiPermission User
- * 
- * @apiParam {Boolean} A value to indiciate if the user decide to rsvp
+ *
+ * @apiParam {Boolean} A value to indicate if the user decide to rsvp
  *
  * @apiUse AuthArgumentRequired
  * @apiSuccess sends a email with user's pin
  * @apiUse IllegalArgumentError
  */
- route.post('/rsvp', (req, res, next) => {
-  if (req.body && (req.body.rsvp != null)){
-    if (res.locals.user){
-      database.setRSVP(res.locals.users.uid,req.body.rsvp)
-      .then(() => {
-        if (req.body.rsvp === true){
-          database.getRegistration(res.locals.users.uid)
-          .on('data', (data) => {..
-            let email = data.email;
-            let name = data.firstname + data.lastname;
-            let pin = data.pin;
-            let promise = new Promise((resolve) => {
-              functions.emailSubstitute(constans.RSVPEmailHtml.text, name,[{NAME: name, PIN: pin}]).then((subbedHTML) => {
-                const request = functions.createEmailRequest(email, subbedHTML, constants.RSVPEmailHtml.subject, "");
-                functions.sendEmail(request.data)
-                  .then(() => {
-                    resolve({'email': request.data.to, 'html': request.data.htmlContent, 'response': 'success'});
-                  }).catch((err) => {
-                    const error = new Error();
-                    error.status = 500;
-                    error.body = err.message;
-                    next(error);
-                  })
-              })
-            })
-          })
-        }
-      })
-      .catch((err) => {
+router.post('/rsvp', (req, res, next) => {
+  if (req.body && (req.body.rsvp !== null)) {
+    if (res.locals.user) {
+      database.setRSVP(res.locals.user.uid, req.body.rsvp)
+        .then(() => {
+          if (req.body.rsvp === true) {
+            database.getRegistration(res.locals.user.uid)
+              .on('data', (data) => {
+                let email = data.email;
+                let name = data.firstname + data.lastname;
+                let pin = data.pin;
+                functions.emailSubstitute(constants.RSVPEmailHtml.text, name, [{
+                  name: name,
+                  pin: parseInt(pin, 10).toString(14),
+                }])
+                  .then((subbedHTML) => {
+                    const request = functions.createEmailRequest(email, subbedHTML, constants.RSVPEmailHtml.subject, "");
+                    functions.sendEmail(request.data)
+                      .then(() => {
+                        res.status(200).send({message: 'success', pin: parseInt(pin, 10).toString(14)});
+                        // resolve({'email': request.data.to, 'html': request.data.htmlContent, 'response': 'success'});
+                      })
+                      .catch((err) => { // Send Email error
+                        const error = new Error();
+                        error.status = 500;
+                        error.body = err.message;
+                        next(error);
+                      });
+                  }).catch((err) => { // Email Substitute error
+                  const error = new Error();
+                  error.status = 500;
+                  error.body = err.message;
+                  next(error);
+                });
+              }).on('err', (err) => {
+              const error = new Error();
+              error.status = 500;
+              error.body = err.message;
+              next(error);
+            });
+          }
+        }).catch((err) => {
         const error = new Error();
         error.status = 500;
         error.body = err.message;
         next(error);
-       })
-    } else{
-        const error = new Error();
-        error.status = 400
-        error.body = {error: 'Could not identify user'};
-        next(error);
-      } 
+      });
+    } else {
+      const error = new Error();
+      error.status = 400;
+      error.body = {error: 'Could not identify user'};
+      next(error);
+    }
   } else {
     const error = new Error();
     error.status = 400;
     error.body = {error: 'RSVP value must be included'};
     next(error);
   }
-}
-  
+})
+;
+
 
 module.exports = router;

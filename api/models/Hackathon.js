cd902d772dae49793056d618ad6bc21cb15710a6
Sushrut Shringarputale <sushrut.shringarputale@leanplum.com>
2018-07-14 17:50:01 -0700

    Fix tests.

--- api/models/Hackathon.js
+++ api/models/Hackathon.js
@@ -11,6 +11,7 @@ const squelOptions = {
   autoQuoteTableNames: true,
   autoQuoteFieldNames: true,
 };
+const RegTableName = 'REGISTRATION';
 
 module.exports.Hackathon = class Hackathon extends BaseObject {
   /**
@@ -65,27 +66,12 @@ module.exports.Hackathon = class Hackathon extends BaseObject {
   static getCount(uow) {
     return super.getCount(uow, TABLE_NAME, COLUMN_NAME);
   }
+
   /**
    * Adds a new hackathon. Validates the data and begins a transaction
    *
    * @return {Promise<ResultSet>}
    */
-/*
-  static getActiveHackathon(uow) {
-    const query = squel.select(squelOptions)
-      .field('uid')
-      .field('name')
-      .field('base_pin')
-      .from(TABLE_NAME)
-      .where('active = ?', true)
-      .toParam();
-    query.text = query.text.concat(';');
-    return uow.query(query.text, query.values);
-  }
-
-  add() {
-    this.active = true;
-    */
   add() {
     const validation = this.validate();
     if (!validation.result) {
@@ -95,94 +81,13 @@ module.exports.Hackathon = class Hackathon extends BaseObject {
       }
       return Promise.reject(new Error(validation.error));
     }
-  /*
-    // Create a new connection and begin a transaction
-    const activeQuery = squel.update(squelOptions)
-      .table(TABLE_NAME)
-      .set('active', false)
-      .set('end_time', new Date().getTime().toString())
-      .where('active = ?', true)
-      .toParam();
-    const newHackathonQuery = squel.insert(squelOptions)
-      .into(TABLE_NAME)
-      .setFieldsRows([this._dbRepresentation])
-      .set(
-        'base_pin',
-        squel.select({
-          autoQuoteTableNames: false,
-          autoQuoteFieldNames: false,
-        })
-          .from('REGISTRATION FOR UPDATE')
-          .field('MAX(pin)'),
-      )
-      .toParam();
-    const query = {
-      text: activeQuery.text.concat(';').concat(newHackathonQuery.text).concat(';'),
-      values: activeQuery.values.concat(newHackathonQuery.values),
-    };
-    */
-
-    /** -- Deprecated see ActiveHackathon Class --/
-    // Force this hackathon to be the active hackathon
-    if (forceActive) {
-      return new Promise((resolve, reject) => {
-        // Create a new connection and begin a transaction
-        const { connection } = this.uow;
-        connection.connection.connectAsync()
-          .then(() => {
-            const { pool } = connection.connection;
-            pool.getConnection((err, poolConnection) => {
-              if (err) {
-                return reject(err);
-              }
-              poolConnection.beginTransaction((err) => {
-                if (err) {
-                  return reject(err);
-                }
-                const activeQuery = squel.update(squelOptions)
-                  .table(TABLE_NAME)
-                  .set('active', false)
-                  .set('end_time', new Date().getTime().toString())
-                  .where('active = ?', true)
-                  .toParam();
-                const newHackathonQuery = squel.insert(squelOptions)
-                  .into(TABLE_NAME)
-                  .setFieldsRows([this._dbRepresentation])
-                  .set(
-                    'base_pin',
-                    squel.select()
-                      .from('REGISTRATION')
-                      .field('MAX(pin)'),
-                  )
-                  .toParam();
-                const query = {
-                  text: activeQuery.text.concat(';').concat(newHackathonQuery.text).concat(';'),
-                  values: activeQuery.values.concat(newHackathonQuery.values),
-                };
-                poolConnection.query(query.text, query.values, (err) => {
-                  if (err) {
-                    poolConnection.rollback();
-                    return reject(err);
-                  }
-                  poolConnection.commit(() => poolConnection.release());
-                  return resolve(this._dbRepresentation);
-                });
-                return null;
-              });
-              return null;
-            });
-          });
-      });
-    }
-    **/
-    let RegTableName = 'REGISTRATION';
     const query = squel.insert(squelOptions)
       .into(this.tableName)
       .setFieldsRows([this._dbRepresentation])
       .set(
         'base_pin',
         squel.select({ autoQuoteFieldNames: false, autoQuoteTableNames: true })
-          .from(RegTableName)
+          .from(`${RegTableName} FOR SHARE`)
           .field('MAX(pin)'),
       )
       .toParam();

13a26d7b304ab3f7bace5fb9294ef5b18201a870
David McDermott <davidsamuelmcdermott@gmail.com>
2018-09-02 18:20:23 -0400

    Updated & Documented assignTable.sql

--- api/assets/database/tableAssignment/assignTable.sql
+++ api/assets/database/tableAssignment/assignTable.sql
@@ -1,11 +1,36 @@
+/* assignTable procedure
+ *
+ * Assigns a table to a given group project with category requirements.  
+ * 
+ * In:
+ * 	projectID_param (char(36)): The UID for a project within a given hackathon.
+ *  categoryID_param (int(11)): The category ID for the project.
+ * Out:
+ *  tableNumber_param (int(11)): The table number assigned to the project.
+ *
+ * Requires:
+ *  `select ? for update` locking syntax
+ *
+ * Warnings:
+ *	If the query does not successfully return, the selected table will deadlock
+ *
+*/
 CREATE PROCEDURE `assignTable`(in projectID_param char(36), in categoryID_param int(11), out tableNumber_param int(11))
 begin
+	declare hackathon_var varchar(45);
+	
+	--Get the current hackathon ID
+	set hackathon_var := (select `uid` from `HACKATHON` where `active` = 1);
+	--Pick a table number not in TABLE_ASSIGNMENTS & lock the table being assigned
     set tableNumber_param := 
         (select tableID from TABLE_SUPPORT 
-            where categoryID = categoryID_param and tableID not in (select tableNumber from TABLE_ASSIGNMENTS)
+            where categoryID = categoryID_param and tableID not in (select tableNumber from TABLE_ASSIGNMENTS where hackathon = hackathon_var)
                 order by priority desc limit 1 for update);
+	--Check for null table number - this indicates that no acceptable tables are available
     if tableNumber_param is not null then
+		--Update to TABLE_ASSIGNMENTS to have the association
         insert into TABLE_ASSIGNMENTS (tableNumber, projectID) values (tableNumber_param, projectID_param);
     end if;
+	--NOP to release the lock on the table
     update TABLE_SUPPORT set tableID = tableNumber_param where tableID = tableNumber_param; 
 end

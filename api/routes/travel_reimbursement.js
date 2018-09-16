811c3f69389fd252af406d52a69203837ba9d77b
Sushrut Shringarputale <contact@sushshring.com>
2018-09-15 23:28:42 -0400

    Added tests and changed testing scripts to use staging authentication

--- api/routes/travel_reimbursement.js
+++ api/routes/travel_reimbursement.js
@@ -1,16 +1,12 @@
 /* eslint-disable max-len */
 const express = require('express');
 const path = require('path');
-const Ajv = require('ajv');
-
-const { travelReimbursementSchema } = require('../assets/schemas/load-schemas')(['travelReimbursementSchema']);
 const { TravelReimbursement } = require('../models/TravelReimbursement');
 const { verifyAuthMiddleware } = require('../services/auth');
 const StorageService = require('../services/storage_service');
 const { STORAGE_TYPES } = require('../services/factories/storage_factory');
-const { logger } = require('../services/logging');
+const HttpError = require('../JSCommon/HttpError');
 const constants = require('../assets/constants/constants');
-const { errorHandler500 } = require('../services/functions');
 
 const storage = new StorageService(STORAGE_TYPES.GCS, {
   bucketName: constants.GCS.travelReimbursementBucket,
@@ -18,7 +14,6 @@ const storage = new StorageService(STORAGE_TYPES.GCS, {
     cb(null, generateFileName(req.body.fullName, file));
   },
 });
-const ajv = new Ajv({ allErrors: true });
 
 const router = express.Router();
 
@@ -28,25 +23,14 @@ const router = express.Router();
 const upload = storage.upload({
   fileFilter(req, file, cb) {
     if (path.extname(file.originalname) !== '.jpeg' &&
-      path.extname(file.originalname) !== '.png' &&
-      path.extname(file.originalname) !== '.jpg') {
+        path.extname(file.originalname) !== '.png' &&
+        path.extname(file.originalname) !== '.jpg') {
       return cb(new Error('Only jpeg, jpg, and png are allowed'));
     }
     return cb(null, true);
   },
   limits: { fileSize: 1024 * 1024 * 5 }, // limit to 5MB
 });
-
-/**
- *
- * @param data
- */
-function validateReimbursement(data) {
-  const validate = ajv.compile(travelReimbursementSchema);
-  const result = !!validate(data);
-  logger.error(validate.errors);
-  return result;
-}
 
 /**
  *
@@ -89,15 +73,25 @@ router.use(verifyAuthMiddleware);
  * @apiGroup Travel Reimbursement
  * @apiUse AuthArgumentRequired
  * @apiParam {String} fullName first and last names of the user as they would appear on a check.
- * @apiParam {Number} reimbursementAmount the total amount of money they are requesting, as appears on their receipts
+ * @apiParam {Number} reimbursementAmount the total amount of money they are requesting, as appears
+ *   on their receipts
  * @apiParam {String} mailingAddress the full postal address of the user
  * @apiParam {enum} groupMembers ["1", "2", "3", "4+"]
- * @apiParam {FILE} [receipt] The receipt files for this user, users can send up to 5 files all under fieldname receipt. (Max size: 5 MB each)
+ * @apiParam {FILE} [receipt] The receipt files for this user, users can send up to 5 files all
+ *   under fieldname receipt. (Max size: 5 MB each)
  * @apiPermission UserPermission
  * @apiSuccess {String} Success
  * @apiUse IllegalArgumentError
  */
 router.post('/', upload.array('receipt', 5), (req, res, next) => {
+  if (!req.body ||
+      !req.body.reimbursementAmount ||
+      !req.files ||
+      !req.body.mailingAddress ||
+      !req.body.groupMembers) {
+    const error = new HttpError('Malformed body', 400);
+    return next(error);
+  }
   if (!parseInt(req.body.reimbursementAmount, 10)) {
     const error = new Error();
     error.body = { error: 'Reimbursement amount must be a number' };
@@ -105,12 +99,6 @@ router.post('/', upload.array('receipt', 5), (req, res, next) => {
     return next(error);
   }
   req.body.reimbursementAmount = parseInt(req.body.reimbursementAmount, 10);
-  // if (!req.body || !validateReimbursement(req.body)) {
-  //   const error = new Error();
-  //   error.body = { error: 'Request body must be set and be valid' };
-  //   error.status = 400;
-  //   return next(error);
-  // }
   req.body.reimbursementAmount =
     adjustReimbursementPrice(req.body.reimbursementAmount, req.body.groupMembers);
   req.body.receiptURIs = req.files.map(({ filename }) => storage.uploadedFileUrl(filename))

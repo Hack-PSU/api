3858248fd4193499136a188ced35671e675bff6c
Sushrut Shringarputale <sush.shring@gmail.com>
2018-09-18 20:24:24 +0530

    Bug fixes (#62)
    * Quick bugfix for user_data route
    
    * Bug fixes on scanner route

--- api/services/database.js
+++ api/services/database.js
@@ -1,6 +1,9 @@
 const squel = require('squel');
 const uuidv4 = require('uuid/v4');
 const Hackathon = require('../models/Hackathon');
+const PreRegistration = require('../models/PreRegistration');
+const Registration = require('../models/Registration');
+const RSVP = require('../models/RSVP');
 
 
 /**
@@ -130,32 +133,24 @@ function addRfidScans(uow, scans) {
  * @return {Promise<any>}
  */
 function getAllUsersList(uow) {
-  let query = squel.select({ autoQuoteTableNames: true, autoQuoteFieldNames: false })
-    .from(squel.select(({ autoQuoteTableNames: true, autoQuoteFieldNames: true }))
-      .from('REGISTRATION', 'r')
-      .field('r.uid')
-      .join(Hackathon.TABLE_NAME, 'h', 'r.hackathon = h.uid and h.active = 1')
-      .union(squel.select(({ autoQuoteTableNames: true, autoQuoteFieldNames: true }))
-        .from('RSVP', 'rsvp')
-        .field('rsvp.user_id')
-        .join(Hackathon.TABLE_NAME, 'h', 'rsvp.hackathon = h.uid and h.active = 1'))
-      .union(squel.select(({ autoQuoteTableNames: true, autoQuoteFieldNames: true }))
-        .from('RFID_ASSIGNMENTS', 'rfid')
-        .field('rfid.user_uid')
-        .join(Hackathon.TABLE_NAME, 'h', 'rfid.hackathon = h.uid and h.active = 1')), 'a')
-    .left_join('REGISTRATION', 'r', 'a.uid = r.uid')
-    .left_join('RSVP', 'v', 'a.uid = v.user_id')
-    .left_join('RFID_ASSIGNMENTS', 'f', 'a.uid = f.user_uid')
-    .left_join('PRE_REGISTRATION', 'p', 'r.email = p.email')
-    .field('r.*')
-    .field(`r.pin - (${squel.select({ autoQuoteTableNames: true, autoQuoteFieldNames: true })
-      .field('base_pin')
-      .from(Hackathon.TABLE_NAME, 'h')
-      .where('active = 1')
-      .toString()})`, 'pin')
-    .field('p.uid', 'pre_uid')
-    .field('v.user_id')
-    .field('f.user_uid')
+  let query = squel.select({ autoQuoteFieldNames: false, autoQuoteTableNames: true })
+    .field('pre_reg.uid', 'pre_uid')
+    .field('reg.*')
+    .field('reg.pin - hackathon.base_pin', 'pin')
+    .field('hackathon.name')
+    .field('hackathon.start_time')
+    .field('hackathon.end_time')
+    .field('hackathon.base_pin')
+    .field('hackathon.active')
+    .field('rsvp.user_id')
+    .field('rsvp.rsvp_time')
+    .field('rsvp.rsvp_status')
+    .field('rfid.user_uid')
+    .from(PreRegistration.TABLE_NAME, 'pre_reg')
+    .right_join(Registration.TABLE_NAME, 'reg', 'pre_reg.email = reg.email')
+    .join(Hackathon.TABLE_NAME, 'hackathon', 'reg.hackathon = hackathon.uid and hackathon.active = 1')
+    .left_join(RSVP.TABLE_NAME, 'rsvp', 'reg.uid = rsvp.user_id AND rsvp.hackathon = hackathon.uid')
+    .left_join('RFID_ASSIGNMENTS', 'rfid', 'reg.uid = rfid.user_uid AND hackathon.uid = rfid.hackathon')
     .toString();
   query = query.concat(';');
   return uow.query(query, null, { stream: true });

4cefb51bbd2b86d47ec1570ced39298beeaaead6
Sushrut Shringarputale <contact@sushshring.com>
2018-12-07 18:22:27 -0500

    Cleaned up GCS Storage Engine
    Signed-off-by: Sushrut Shringarputale <contact@sushshring.com>

--- api/services/factories/GCSStorageEngine.ts
+++ api/services/factories/GCSStorageEngine.ts
@@ -1,65 +1,79 @@
-import * as storage from '@google-cloud/storage';
-import * as uuid from 'uuid';
+import * as Storage from '@google-cloud/storage';
+import * as express from 'express';
+import * as multer from 'multer';
 
-// TODO: Cleanup
-export class GCSStorageEngine {
-  private gcobj: storage.Storage;
-  private gcsBucket: storage.Bucket;
-  private options: any;
+export class GCSStorageEngine implements multer.StorageEngine {
+  private gcobj: Storage.Storage;
+  private gcsBucket: Storage.Bucket;
+  private readonly filenameGenerator: (req: express.Request, file: any) => string;
+  private metadata: Storage.WriteStreamOptions;
 
-  constructor(opts: any = {}) { // TODO: Define opts
-
-    this.getFilename = opts.filename || this.getFilename;
-
-    opts.bucket = opts.bucket || process.env.GCS_BUCKET || null;
-    opts.projectId = opts.projectId || process.env.GCLOUD_PROJECT || null;
-    opts.keyFilename = opts.keyFilename || process.env.GCS_KEYFILE || null;
-
-    if (!opts.bucket) {
-      throw new Error('You have to specify bucket for Google Cloud Storage to work.');
+  constructor(opts: GCSStorageEngine.IGCSStorageEngineOpts) {
+    if (!opts) {
+      throw new Error('Cannot pass null options');
     }
-
-    if (!opts.projectId) {
-      throw new Error('You have to specify project id for Google Cloud Storage to work.');
-    }
-
-    if (!opts.keyFilename) {
-      throw new Error('You have to specify credentials key file for Google Cloud Storage to work.');
-    }
-
     this.gcobj = storage({
       keyFilename: opts.keyFilename,
       projectId: opts.projectId,
     });
     this.gcsBucket = this.gcobj.bucket(opts.bucket);
-    this.options = opts;
-  }
-  public getFilename(req, file, cb) {
-    cb(null, `${uuid()}_${file.originalname}`);
+    this.filenameGenerator = opts.filename || ((req, file1) => file1.fieldname);
+    this.metadata = opts.metadata || {
+      predefinedAcl: 'private',
+    };
   }
 
-  public _handleFile(req, file, cb) {
-    this.getFilename(req, file, (err, filename) => {
-      if (err) {
-        return cb(err);
-      }
+  public _handleFile(
+    req: express.Request,
+    file: IStorageService.IFile,
+    cb: (error?: Error, info?: Partial<IStorageService.IFile>) => void,
+  ) {
+    try {
+      const filename = this.filenameGenerator(req, file);
       const gcFile = this.gcsBucket.file(filename);
-      return file.stream.pipe(gcFile.createWriteStream({
-        metadata: this.options.metadata,
-        predefinedAcl: this.options.acl || 'private',
-      }))
+      return file.stream.pipe(gcFile.createWriteStream(this.metadata))
         .on('error', (err) => cb(err))
         .on('finish', () => cb(null, {
           filename,
-          path: `https://${this.options.bucket}.storage.googleapis.com/${filename}`,
+          path: `https://${this.gcsBucket.name}.storage.googleapis.com/${filename}`,
         }));
-    });
+    } catch (error) {
+      cb(error);
+    }
   }
 
-  public _removeFile(req, file, cb) {
+  public _removeFile(
+    req: express.Request,
+    file: IStorageService.IFile,
+    cb: (error: Error) => void,
+  ) {
     const gcFile = this.gcsBucket.file(file.filename);
     gcFile.delete()
-      .then(data => cb(null, data))
-      .catch(err => cb(err, null));
+      .then(() => cb(null))
+      .catch(err => cb(err));
   }
-};
+}
+
+declare global {
+  namespace GCSStorageEngine {
+    interface IGCSStorageEngineOpts {
+      /*
+       GCS Bucket to use
+       */
+      bucket: string;
+      /*
+       Project on Google Cloud platform
+       */
+      projectId: string;
+      /*
+       Name of file containing private key
+       */
+      keyFilename: string;
+      /*
+       Name of file to upload
+       */
+      filename?: (req: express.Request, file: any) => string;
+      metadata?: Storage.WriteStreamOptions;
+    }
+  }
+}

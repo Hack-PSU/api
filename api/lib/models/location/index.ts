f87af9bc0df376bfcf3167909db96dda5acc83a4
Michael Cao <mike.x.cao@gmail.com>
2019-02-10 17:40:05 -0500

    Copied Event models over to Location and migrated Location datatype

---
+++ api/lib/models/location/index.ts
@@ -0,0 +1,20 @@
+import { Stream } from "ts-stream";
+import { UidType } from "../../JSCommon/common-types";
+import { IDataMapper, IDbResult } from "../../services/database";
+import { Location } from "./location";
+//
+// interface IEventDataMapper extends IDataMapper {
+//   get(id: UidType): Promise<IDbResult<Event>>;
+//
+//   insert(object: Event): Promise<IDbResult<Event>>;
+//
+//   update(object: Event): Promise<IDbResult<Event>>;
+//
+//   delete(id: UidType): Promise<IDbResult<void>>;
+//
+//   getAll(): Promise<IDbResult<Stream<Event>>>;
+//
+//   getCount(): Promise<IDbResult<number>>;
+// }
+
+export { Location };
---
+++ api/lib/models/location/location-data-mapper-impl.ts
@@ -0,0 +1,170 @@
+import { Inject, Injectable } from "injection-js";
+import { from } from "rxjs";
+import { map } from "rxjs/operators";
+import * as squel from "squel";
+import { Stream } from "ts-stream";
+import { UidType } from "../../JSCommon/common-types";
+import { HttpError } from "../../JSCommon/errors";
+import { AuthLevel } from "../../services/auth/auth-types";
+import { IAcl, IAclPerm } from "../../services/auth/RBAC/rbac-types";
+import { IDataMapper, IDbResult } from "../../services/database";
+import { GenericDataMapper } from "../../services/database/svc/generic-data-mapper";
+import { MysqlUow } from "../../services/database/svc/mysql-uow.service";
+import { IUowOpts } from "../../services/database/svc/uow.service";
+import { Logger } from "../../services/logging/logging";
+import { Location } from "./location";
+
+@Injectable()
+export class LocationDataMapperImpl extends GenericDataMapper
+  implements IDataMapper<Location>, IAclPerm {
+  // ACL permissions
+  public readonly CREATE: string = "location:create";
+  public readonly DELETE: string = "location:delete";
+  public readonly READ: string = "location:read";
+  public readonly UPDATE: string = "location:update";
+  public readonly READ_ALL: string = "location:readall";
+  public readonly COUNT: string = "location:count";
+  public tableName = "LOCATIONS";
+
+  protected pkColumnName = "uid";
+
+  constructor(
+    @Inject("IAcl") acl: IAcl,
+    @Inject("MysqlUow") protected readonly sql: MysqlUow,
+    @Inject("BunyanLogger") protected readonly logger: Logger
+  ) {
+    super(acl);
+    super.addRBAC(
+      [this.CREATE, this.UPDATE, this.DELETE],
+      [AuthLevel.TEAM_MEMBER],
+      undefined,
+      [AuthLevel[AuthLevel.VOLUNTEER]]
+    );
+    super.addRBAC([this.READ, this.READ_ALL], [AuthLevel.PARTICIPANT]);
+  }
+
+  public delete(id: UidType): Promise<IDbResult<void>> {
+    const query = squel
+      .delete({ autoQuoteTableNames: true, autoQuoteFieldNames: true })
+      .from(this.tableName)
+      .where(`${this.pkColumnName} = ?`, id)
+      .toParam();
+    query.text = query.text.concat(";");
+    return from(
+      this.sql.query(query.text, query.values, { stream: false, cache: false })
+    )
+      .pipe(map(() => ({ result: "Success", data: undefined })))
+      .toPromise();
+  }
+
+  public get(id: UidType, opts?: IUowOpts): Promise<IDbResult<Location>> {
+    let queryBuilder = squel
+      .select({ autoQuoteFieldNames: true, autoQuoteTableNames: true })
+      .from(this.tableName);
+    if (opts && opts.fields) {
+      queryBuilder = queryBuilder.fields(opts.fields);
+    }
+    queryBuilder = queryBuilder.where(`${this.pkColumnName}= ?`, id);
+    const query = queryBuilder.toParam();
+    query.text = query.text.concat(";");
+    return from(
+      this.sql.query<Location>(query.text, query.values, {
+        stream: false,
+        cache: true
+      })
+    )
+      .pipe(
+        map((location: Location) => ({ result: "Success", data: location }))
+      )
+      .toPromise();
+  }
+
+  public getAll(): Promise<IDbResult<Stream<Location>>> {
+    const query = squel
+      .select({ autoQuoteTableNames: true, autoQuoteFieldNames: true })
+      .from(this.tableName, "location")
+      .field("location.*")
+      .field("location.location_name")
+      .join("HACKATHON", "h", "h.uid=location.hackathon and h.active=true")
+      .toString()
+      .concat(";");
+    return from(
+      this.sql.query<Location>(query, [], { stream: true, cache: true })
+    )
+      .pipe(
+        map((location: Stream<Location>) => ({
+          result: "Success",
+          data: location
+        }))
+      )
+      .toPromise();
+  }
+
+  public getCount(): Promise<IDbResult<number>> {
+    const query = squel
+      .select({ autoQuoteTableNames: true, autoQuoteFieldNames: false })
+      .from(this.tableName)
+      .field(`COUNT(${this.pkColumnName})`, "count")
+      .toString()
+      .concat(";");
+    const params = [];
+    return from(
+      this.sql.query<number>(query, params, { stream: true, cache: true })
+    )
+      .pipe(map((result: number) => ({ result: "Success", data: result })))
+      .toPromise();
+  }
+
+  public insert(object: Location): Promise<IDbResult<Location>> {
+    const validation = object.validate();
+    if (!validation.result) {
+      this.logger.warn("Validation failed while adding object.");
+      this.logger.warn(object.dbRepresentation);
+      return Promise.reject({
+        result: "error",
+        data: new HttpError(validation.error, 400)
+      });
+    }
+    const query = squel
+      .insert({ autoQuoteFieldNames: true, autoQuoteTableNames: true })
+      .into(this.tableName)
+      .setFieldsRows([object.dbRepresentation])
+      .toParam();
+    query.text = query.text.concat(";");
+    return from(
+      this.sql.query<void>(query.text, query.values, {
+        stream: false,
+        cache: false
+      })
+    )
+      .pipe(map(() => ({ result: "Success", data: object })))
+      .toPromise();
+  }
+
+  public update(object: Location): Promise<IDbResult<Location>> {
+    const validation = object.validate();
+    if (!validation.result) {
+      this.logger.warn("Validation failed while adding object.");
+      this.logger.warn(object.dbRepresentation);
+      return Promise.reject({
+        result: "error",
+        data: new HttpError(validation.error, 400)
+      });
+    }
+    const query = squel
+      .update({ autoQuoteFieldNames: true, autoQuoteTableNames: true })
+      .table(this.tableName)
+      .setFields(object.dbRepresentation)
+      .where(`${this.pkColumnName} = ?`, object.id)
+      .toParam();
+    query.text = query.text.concat(";");
+    return from(
+      this.sql.query<void>(query.text, query.values, {
+        stream: false,
+        cache: false
+      })
+    )
+      .pipe(map(() => ({ result: "Success", data: object })))
+      .toPromise();
+  }
+}

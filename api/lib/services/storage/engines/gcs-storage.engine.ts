38a4e1911e3d174828d81d8ee41495ffe6d2c638
Michael Cao <mike.x.cao@gmail.com>
2019-02-10 21:20:53 -0500

    Fixed interface and storage issues

--- api/lib/services/storage/engines/gcs-storage.engine.ts
+++ api/lib/services/storage/engines/gcs-storage.engine.ts
@@ -1,4 +1,4 @@
-import storage from '@google-cloud/storage';
+import Storage from '@google-cloud/storage';
 import * as express from 'express';
 import * as multer from 'multer';
 import { IFile, IGcsStorageEngineOpts } from '../storage-types';
--- api/lib/services/storage/storage-types/index.ts
+++ api/lib/services/storage/storage-types/index.ts
@@ -1,4 +1,4 @@
-import storage from '@google-cloud/storage';
+import Storage from '@google-cloud/storage';
 import * as express from 'express';
 
 export interface IFileUploadLimits {

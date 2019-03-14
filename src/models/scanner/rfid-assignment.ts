6c8a0b7bc50dabea04825f07632174144451593d
Sushrut Shringarputale <contact@sushshring.com>
2019-03-14 15:39:24 -0400

    Assignments and Scans return wid instead of rfid_uid now

--- src/models/scanner/rfid-assignment.ts
+++ src/models/scanner/rfid-assignment.ts
@@ -38,4 +38,10 @@ export class RfidAssignment extends BaseObject {
     this.hackathon = data.hackathon;
   }
 
+  public get cleanRepresentation() {
+    const repr: any = super.cleanRepresentation;
+    repr.wid = repr.rfid_uid;
+    delete repr.rfid_uid;
+    return repr as this;
+  }
 }

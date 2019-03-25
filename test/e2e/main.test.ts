5ba408b16b751d7ca9e305550a1e8ff9b8934580
Sushrut Shringarputale <contact@sushshring.com>
2019-03-24 21:52:56 -0400

    Heres the actual change lmao

--- test/e2e/main.test.ts
+++ test/e2e/main.test.ts
@@ -1,14 +1,5 @@
-process.env.APP_ENV = 'test';
-import * as chai from 'chai';
-import chaiHttp from 'chai-http';
-import 'mocha';
-import app from '../../src/app';
-
-import test_helper from '../test_helper';
-
-chai.use(chaiHttp);
-
-test_helper();
+import { app, chai, testHelper } from '../test_helper';
+testHelper();
 
 const { expect } = chai;
 
@@ -20,8 +11,14 @@ describe('INTEGRATION TEST: /', () => {
     const res = await chai.request(app).get('/');
     // THEN: Responds with success message
     expect(res).status(200);
-    expect(res).header('\'content-type\', /^application\\/json.*/');
-    expect(res.body).to.deep.equal({});
-    res.should.have.status(200);
+    expect(res).header('content-type', 'application/json; charset=utf-8');
+    expect(res.body).to.deep.equal({
+      api_response: 'Welcome to the HackPSU API!',
+      body: {
+        data: {},
+        result: 'Success',
+      },
+      status: 200,
+    });
   });
 });

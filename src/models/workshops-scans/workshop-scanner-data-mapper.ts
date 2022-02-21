1fbeb980f6f82531feab0aecbab8607a6d92a8ad
John <john.m.durrell@gmail.com>
2022-02-21 02:13:22 -0500

    Prod push: Workshop Scan and Image Endpoint (#187)
    * Jdurrell/fix 400 could not find image error (#183)
    
    * Prod Push (#170)
    
    Co-authored-by: Stan <stanleykwok61@gmail.com>
    Co-authored-by: jdurrell <john.m.durrell.gmail.com>
    Co-authored-by: John <john.m.durrell@gmail.com>
    Co-authored-by: stan29308 <35966427+stan29308@users.noreply.github.com>
    
    * remove incorrect 400 error message
    
    * changed image bucket to hackpsu18.appspot.com
    
    Co-authored-by: Rahul Ramkumar <rahulram226@gmail.com>
    Co-authored-by: Stan <stanleykwok61@gmail.com>
    Co-authored-by: stan29308 <35966427+stan29308@users.noreply.github.com>
    
    * workshop-pin-capability-added-pending-update (#180)
    
    * workshop-pin-capability-added-pending-update
    
    * cleaning up copy-paste bloat and implementing infrastructure
    
    * updated changes for john
    
    * fixed data mapper and scanner
    
    * update for john
    
    * route integration tests
    
    * documentation and datamapper tests
    
    * bruh, this didn't even compile at first
    
    * fixed permissions, missing idtoken, pin checking, and invalid pin tests
    
    * added workshop scans to test data
    
    * fixed insertion of scans
    
    * fixed get-by-pin integration tests not querying the correct pin
    
    * added option for pin relative to current hackathon
    
    Co-authored-by: jdurrell <john.m.durrell@gmail.com>
    
    Co-authored-by: Rahul Ramkumar <rahulram226@gmail.com>
    Co-authored-by: Stan <stanleykwok61@gmail.com>
    Co-authored-by: stan29308 <35966427+stan29308@users.noreply.github.com>
    Co-authored-by: tcorrellpsu <71111189+tcorrellpsu@users.noreply.github.com>

---
+++ src/models/workshops-scans/workshop-scanner-data-mapper.ts
@@ -0,0 +1,130 @@
+import { Inject, Injectable } from 'injection-js';
+import { default as _ } from 'lodash';
+import { from } from 'rxjs';
+import { map } from 'rxjs/operators';
+import * as squel from 'squel';
+import { MethodNotImplementedError } from '../../JSCommon/errors';
+import { AuthLevel } from '../../services/auth/auth-types';
+import { IAcl, IAclPerm, IWorkshopAclPerm } from '../../services/auth/RBAC/rbac-types';
+import { IDataMapper, IDbResult } from '../../services/database';
+import { GenericDataMapper } from '../../services/database/svc/generic-data-mapper';
+import { MysqlUow } from '../../services/database/svc/mysql-uow.service';
+import { IUowOpts } from '../../services/database/svc/uow.service';
+import { Logger } from '../../services/logging/logging';
+import { IActiveHackathonDataMapper } from '../hackathon/active-hackathon';
+import { IRegisterDataMapper } from '../register';
+import { WorkshopScan } from './workshop-scans';
+import { Hackathon } from 'models/hackathon';
+import { Registration } from 'models/register/registration';
+
+/**
+ * TODO: Add documentation
+ */
+
+ export interface IWorkshopScansDataMapper extends IDataMapper<WorkshopScan> {
+  
+  getByPin(pin: number, hackathon: Hackathon): Promise<IDbResult<Registration>>;
+
+
+ }
+@Injectable()
+export class WorkshopDataMapperImpl extends GenericDataMapper
+  implements IAclPerm, IWorkshopAclPerm, IWorkshopScansDataMapper {
+
+  get tableName() {
+    return this.TABLE_NAME;
+  }
+
+  public readonly READ: string = 'registration:read';
+  public readonly CREATE: string = 'attendance:create';
+  public readonly DELETE: string = 'attendance:delete';
+  public readonly READ_ALL: string = 'registration:read';
+  public readonly UPDATE: string = 'attendance:create';
+  public readonly COUNT: string = 'attendance:count';
+
+  public readonly CHECK_IN: string = 'workshop:checkin';
+  
+  protected readonly pkColumnName: string = 'uid';
+  protected readonly TABLE_NAME = 'WORKSHOP_SCANS';
+
+  constructor(
+    @Inject('IAcl') acl: IAcl,
+    @Inject('MysqlUow') protected readonly sql: MysqlUow,
+    @Inject('IActiveHackathonDataMapper') protected readonly activeHackathonDataMapper: IActiveHackathonDataMapper,
+    @Inject('IRegisterDataMapper') protected readonly registerDataMapper: IRegisterDataMapper,
+    @Inject('BunyanLogger') protected readonly logger: Logger,
+  ) {
+    super(acl);
+    // super.addRBAC(
+    //   [this.READ, this.READ_ALL],
+    //   [AuthLevel.TECHNOLOGY],
+    // );
+    // team member verification is required to check in to a workshop
+    super.addRBAC(
+      [this.CHECK_IN],
+      [AuthLevel.TEAM_MEMBER]
+    )
+  }
+
+  // TODO: Should probably move this to the Registration mapper instead
+  public async getByPin(pin: number, hackathon: Hackathon): Promise<IDbResult<Registration>> {
+    const query = squel.select({
+      autoQuoteFieldNames: false,
+      autoQuoteTableNames: true,
+    })
+      .from('REGISTRATION')
+      .where('hackathon = ?', hackathon.uid)
+      .where('pin = ?', hackathon.base_pin! + pin)
+      .toParam();
+    return from(this.sql.query<Registration>(
+      query.text,
+      query.values,
+      { cache: true },
+    ))
+      .pipe(
+        map((registration: Registration[]) => ({ result: 'Success', data: registration[0] })),
+      )
+      .toPromise();
+  }
+
+  public async insert(object: WorkshopScan): Promise<IDbResult<WorkshopScan>> {
+    
+    const query = squel.insert({ autoQuoteFieldNames: true, autoQuoteTableNames: true })
+      .into(this.tableName)
+      .setFieldsRows([object.dbRepresentation])
+      .set(
+        'hackathon_id',
+        await this.activeHackathonDataMapper.activeHackathon.pipe(map(hackathon => hackathon.uid))
+          .toPromise(),
+      )
+      .toParam();
+    query.text = query.text.concat(';');
+    return from(
+      this.sql.query<void>(query.text, query.values, { cache: false }),
+    ).pipe(
+      map(() => ({ result: 'Success', data: object.cleanRepresentation })),
+    ).toPromise();
+  }
+    
+  //Unsupported methods, but forced to be created
+  public async getAll(opts?: IUowOpts): Promise<IDbResult<WorkshopScan[]>> {
+    throw new MethodNotImplementedError('This method is not supported by this class');
+  }
+
+  public async getCount(opts?: IUowOpts): Promise<IDbResult<number>> {
+    throw new MethodNotImplementedError('This method is not supported by this class');
+  }
+
+  public get(uid: string, opts?: IUowOpts): Promise<IDbResult<WorkshopScan>> {
+    throw new MethodNotImplementedError('This method is not supported by this class');
+  }
+
+  public update(object: WorkshopScan, opts?: IUowOpts): Promise<IDbResult<WorkshopScan>> {
+    throw new MethodNotImplementedError('This method is not supported by this class');
+  }
+
+  public delete(uid: string, opts?: IUowOpts): Promise<IDbResult<void>> {
+    throw new MethodNotImplementedError('This method is not supported by this class');
+  }
+
+}
---
+++ test/models/workshop-scanner/workshop-scanner-data-mapper-impl.test.ts
@@ -0,0 +1,112 @@
+import { Substitute } from '@fluffy-spoon/substitute';
+import { expect } from 'chai';
+import 'mocha';
+import { of } from 'rxjs';
+import { anyString, anything, capture, instance, mock, reset, verify, when } from 'ts-mockito';
+import { WorkshopScan } from '../../../src/models/workshops-scans/workshop-scans';
+import { IActiveHackathonDataMapper } from '../../../src/models/hackathon/active-hackathon';
+import { ActiveHackathon } from '../../../src/models/hackathon/active-hackathon/active-hackathon';
+import { RBAC } from '../../../src/services/auth/RBAC/rbac';
+import { IAcl } from '../../../src/services/auth/RBAC/rbac-types';
+import { MysqlUow } from '../../../src/services/database/svc/mysql-uow.service';
+import { Logger } from '../../../src/services/logging/logging';
+import { IWorkshopScansDataMapper, WorkshopDataMapperImpl } from '../../../src/models/workshops-scans/workshop-scanner-data-mapper';
+import { IRegisterDataMapper } from '../../../src/models/register';
+import { RegisterDataMapperImpl } from '../../../src/models/register/register-data-mapper-impl';
+
+let workshopScansDataMapper: WorkshopDataMapperImpl;
+let registerDataMapper: IRegisterDataMapper;
+let activeHackathonDataMapper: IActiveHackathonDataMapper;
+let mysqlUow: MysqlUow;
+const mysqlUowMock = mock(MysqlUow);
+const acl: IAcl = new RBAC();
+const testHackathonUid = 'test uid';
+
+describe('TEST: Workshop Scanner Data Mapper', () => {
+
+    beforeEach(() => {
+      // Configure Active Hackathon Data Mapper
+      activeHackathonDataMapper = Substitute.for<IActiveHackathonDataMapper>();
+      (activeHackathonDataMapper.activeHackathon as any).returns(of(new ActiveHackathon({
+        basePin: 0,
+        endTime: null,
+        name: 'test hackathon',
+        uid: testHackathonUid,
+      })));
+      (activeHackathonDataMapper.tableName as any).mimicks(() => 'HACKATHON');
+      // Configure Mock MysqlUow
+      when(mysqlUowMock.query(anyString(), anything(), anything()))
+        .thenResolve([]);
+      mysqlUow = instance(mysqlUowMock);
+      // Configure Workshop Scans Data Mapper
+      workshopScansDataMapper = new WorkshopDataMapperImpl(acl, mysqlUow, activeHackathonDataMapper, registerDataMapper, new Logger());
+    });
+  
+    afterEach(() => {
+      reset(mysqlUowMock);
+    });
+
+    describe('TEST: WorkshopScan get user by pin', () => {
+        beforeEach(() => {
+        when(mysqlUowMock.query(anyString(), anything(), anything()))
+            .thenResolve([{}]);
+        mysqlUow = instance(mysqlUowMock);
+        // Configure Register Data Mapper
+        registerDataMapper = new RegisterDataMapperImpl(
+            acl,
+            mysqlUow,
+            activeHackathonDataMapper,
+            new Logger(),
+        );
+        });
+        afterEach(() => {
+        reset(mysqlUowMock);
+        });
+        // @ts-ignore
+        it('generates the correct sql to get the specific registration from the registration table', async () => {
+        // GIVEN: A valid pin
+        const pin = 12345;
+        // WHEN: The current registration version is retrieved
+        await registerDataMapper.getByPin(pin, testHackathonUid);
+        // THEN: Generated SQL matches the expectation
+        const expectedSQL = 'SELECT * FROM `REGISTRATION` WHERE (hackathon = ?) AND (pin = ?)';
+        const expectedParams = [testHackathonUid, pin];
+        const [generatedSQL, generatedParams] = capture<string, string[]>(mysqlUowMock.query)
+            .first();
+        verify(mysqlUowMock.query(anything(), anything(), anything())).once();
+        expect(generatedSQL).to.equal(expectedSQL);
+        expect(generatedParams).to.deep.equal(expectedParams);
+        });
+    });
+
+    describe('TEST: WorkshopScan insert', () => {
+        // @ts-ignore
+        it('inserts attendance for an event', async () => {
+        // GIVEN: A workshop scan to insert
+        const testWorkshopScan = new WorkshopScan({
+            eventUid: 'test event id',
+            hackathonUid: testHackathonUid,
+            timestamp: Date.now(),
+            pin: 12345,
+        });
+        // WHEN: Inserting the workshop scan
+        await workshopScansDataMapper.insert(testWorkshopScan);
+
+        // THEN: Generated SQL matches the expectation
+        const expectedSQL = 'INSERT INTO `WORKSHOP_SCANS` (`event_id`, `hackathon_id`, `timestamp`, `user_pin`) VALUES (?, ?, ?, ?);';
+        const expectedParams = [
+            testWorkshopScan.event_id,
+            testWorkshopScan.hackathon_id,
+            testWorkshopScan.timestamp,
+            testWorkshopScan.user_pin,
+        ];
+        const [generatedSQL, generatedParams] = capture<string, any[]>(mysqlUowMock.query)
+            .first();
+        verify(mysqlUowMock.query(anything(), anything(), anything())).once();
+        expect(generatedSQL).to.equal(expectedSQL);
+        expect(generatedParams).to.deep.equal(expectedParams);
+        });
+    });
+
+    
+});

7d8ecc00f82ccd6299b870d8e416e9de73e90146
John <john.m.durrell@gmail.com>
2022-04-08 00:44:08 -0400

    Jdurrell/error on duplicate checkin (#213)
    * check for null firebase custom claims
    
    * stupid merge
    
    * reject duplicate workshop checkin

--- src/router/router-types/response-body.ts
+++ src/router/router-types/response-body.ts
@@ -3,9 +3,9 @@ import { IDbResult } from '../../services/database';
 export class ResponseBody {
   public readonly api_response: string;
   public readonly status: number;
-  public readonly body: IDbResult<any>;
+  public readonly body: IDbResult<any> | undefined;
 
-  constructor(response: string, status: number, body: IDbResult<any>) {
+  constructor(response: string, status: number, body?: IDbResult<any>) {
     this.api_response = response;
     this.status = status;
     this.body = body;

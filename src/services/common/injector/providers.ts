bf41586464d5dc4c26ac3ae53b0d98d8adf4c319
jdurrell <john.m.durrell@gmail.com>
2022-07-16 12:51:23 -0400

    Alphabetized injection providers.

--- src/services/common/injector/providers.ts
+++ src/services/common/injector/providers.ts
@@ -1,4 +1,4 @@
-import { WorkshopDataMapperImpl } from '../../../models/workshops-scans/workshop-scanner-data-mapper';
+import { ActiveHackathonDataMapperImpl } from '../../../models/hackathon/active-hackathon';
 import { AdminDataMapperImpl } from '../../../models/admin';
 import { AdminStatisticsDataMapperImpl } from '../../../models/admin/statistics/admin-statistics-data-mapper-impl';
 import { AttendanceDataMapperImpl } from '../../../models/attendance/attendance-data-mapper-impl';
@@ -7,16 +7,16 @@ import { CheckoutObjectDataMapperImpl } from '../../../models/checkout-object/ch
 import { EventDataMapperImpl } from '../../../models/event/event-data-mapper-impl';
 import { ExtraCreditDataMapperImpl } from '../../../models/extra-credit/extra-credit-data-mapper-impl';
 import { HackathonDataMapperImpl } from '../../../models/hackathon';
-import { ActiveHackathonDataMapperImpl } from '../../../models/hackathon/active-hackathon';
 import { LocationDataMapperImpl } from '../../../models/location/location-data-mapper-impl';
 import { PreRegisterDataMapperImpl } from '../../../models/register/pre-register-data-mapper-impl';
+import { ProjectDataMapperImpl } from '../../../models/project/project-data-mapper-impl';
 import { RegisterDataMapperImpl } from '../../../models/register/register-data-mapper-impl';
 import { RsvpDataMapperImpl } from '../../../models/RSVP/RSVP-data-mapper-impl';
 import { ScannerDataMapperImpl } from '../../../models/scanner/scanner-data-mapper-impl';
+import { ScoreDataMapperImpl } from '../../../models/score/score-data-mapper';
 import { UpdateDataMapperImpl } from '../../../models/update/update-data-mapper-impl';
 import { UrlDataMapperImpl } from '../../../models/url/url-data-mapper-impl';
-import { ProjectDataMapperImpl } from '../../../models/project/project-data-mapper-impl';
-import { ScoreDataMapperImpl } from '../../../models/score/score-data-mapper';
+import { WorkshopDataMapperImpl } from '../../../models/workshops-scans/workshop-scanner-data-mapper';
 
 import { AdminProcessor } from '../../../processors/admin-processor';
 import { IndexProcessor } from '../../../processors/index-processor';
@@ -25,33 +25,32 @@ import { RegistrationProcessor } from '../../../processors/registration-processo
 import { ScannerProcessor } from '../../../processors/scanner-processor';
 import { UpdateProcessor } from '../../../processors/update-processor';
 
-import { IndexController } from '../../../router/routes';
-import {
-  AdminHackathonController,
-  AdminLocationController,
-  AdminRegisterController,
-  AdminStatisticsController,
-} from '../../../router/routes/admin';
 import { AdminController } from '../../../router/routes/admin/';
 import { AdminCheckoutController } from '../../../router/routes/admin/admin-checkout';
-import { InternalController } from '../../../router/routes/internal';
+import {
+  AdminHackathonController, AdminLocationController, AdminRegisterController, AdminStatisticsController, } from '../../../router/routes/admin';
 import { EventsController } from '../../../router/routes/live/events';
+import { IndexController } from '../../../router/routes';
+import { InternalController } from '../../../router/routes/internal';
+import { JudgingController } from '../../../router/routes/judging';
 import { LiveController } from '../../../router/routes/live/live';
 import { UpdatesController } from '../../../router/routes/live/updates';
+import { UsersController } from '../../../router/routes/users';
 import { ScannerController } from '../../../router/routes/scanner/scanner';
 import { WorkshopScannerController } from '../../../router/routes/scanner/workshop-scanner';
-import { UsersController } from '../../../router/routes/users';
-import { JudgingController } from '../../../router/routes/judging';
-import { FirebaseAuthService } from '../../auth';
+
 import { ApikeyAuthService } from '../../auth/apikey-auth';
+import { FirebaseAuthService } from '../../auth';
 import { RBAC } from '../../auth/RBAC/rbac';
 import { SendgridService } from '../../communication/email/sendgrid.service';
 import { OnesignalService } from '../../communication/push-notification/onesignal.service';
+
 import { MemCacheServiceImpl } from '../../database/cache/memcache-impl.service';
 import { RtdbConnectionFactory } from '../../database/connection/rtdb-connection-factory';
 import { SqlConnectionFactory } from '../../database/connection/sql-connection-factory';
 import { MysqlUow } from '../../database/svc/mysql-uow.service';
 import { RtdbUow } from '../../database/svc/rtdb-uow.service';
+
 import { Logger } from '../../logging/logging';
 import { GoogleStorageService } from '../../storage/svc/google-storage.service';
 import { FirebaseService } from '../firebase/firebase.service';
@@ -63,69 +62,69 @@ export class ExpressProvider {
     RootInjector.registerProvider(
       [
         // Controllers
-        { provide: 'IndexController', useClass: IndexController },
-        { provide: 'LiveController', useClass: LiveController },
-        { provide: 'UpdatesController', useClass: UpdatesController },
-        { provide: 'EventsController', useClass: EventsController },
-        { provide: 'InternalController', useClass: InternalController },
-        { provide: 'UsersController', useClass: UsersController },
+        { provide: 'AdminCheckoutController', useClass: AdminCheckoutController },
         { provide: 'AdminController', useClass: AdminController },
-        { provide: 'ScannerController', useClass: ScannerController },
-        { provide: 'WorkshopController', useClass: WorkshopScannerController},
-        { provide: 'JudgingController', useClass: JudgingController },
-        { provide: 'AdminRegisterController', useClass: AdminRegisterController },
-        { provide: 'AdminStatisticsController', useClass: AdminStatisticsController },
         { provide: 'AdminHackathonController', useClass: AdminHackathonController },
         { provide: 'AdminLocationController', useClass: AdminLocationController },
-        { provide: 'AdminCheckoutController', useClass: AdminCheckoutController },
-
+        { provide: 'AdminRegisterController', useClass: AdminRegisterController },
+        { provide: 'AdminStatisticsController', useClass: AdminStatisticsController },
+        { provide: 'EventsController', useClass: EventsController },
+        { provide: 'IndexController', useClass: IndexController },
+        { provide: 'InternalController', useClass: InternalController },
+        { provide: 'JudgingController', useClass: JudgingController },
+        { provide: 'LiveController', useClass: LiveController },
+        { provide: 'ScannerController', useClass: ScannerController },
+        { provide: 'UpdatesController', useClass: UpdatesController },
+        { provide: 'UsersController', useClass: UsersController },
+        { provide: 'WorkshopController', useClass: WorkshopScannerController},
+        
         // Processors
+        { provide: 'IAdminProcessor', useClass: AdminProcessor },
+        { provide: 'IAdminScannerProcessor', useClass: ScannerProcessor },
         { provide: 'IIndexProcessor', useClass: IndexProcessor },
         { provide: 'IRegistrationProcessor', useClass: RegistrationProcessor },
         { provide: 'IPreregistrationProcessor', useClass: PreRegistrationProcessor },
-        { provide: 'IAdminProcessor', useClass: AdminProcessor },
-        { provide: 'IAdminScannerProcessor', useClass: ScannerProcessor },
         { provide: 'IScannerProcessor', useClass: ScannerProcessor },
         { provide: 'IUpdateProcessor', useClass: UpdateProcessor },
 
         // Data Mappers
-        { provide: 'IUpdateDataMapper', useClass: UpdateDataMapperImpl },
-        { provide: 'IEventDataMapper', useClass: EventDataMapperImpl },
-        { provide: 'ILocationDataMapper', useClass: LocationDataMapperImpl },
-        { provide: 'IAttendanceDataMapper', useClass: AttendanceDataMapperImpl },
-        { provide: 'IExtraCreditDataMapper', useClass: ExtraCreditDataMapperImpl },
-        { provide: 'IRegisterDataMapper', useClass: RegisterDataMapperImpl },
-        { provide: 'IPreRegisterDataMapper', useClass: PreRegisterDataMapperImpl },
-        { provide: 'IRsvpDataMapper', useClass: RsvpDataMapperImpl },
         { provide: 'IActiveHackathonDataMapper', useClass: ActiveHackathonDataMapperImpl },
-        { provide: 'IHackathonDataMapper', useClass: HackathonDataMapperImpl },
         { provide: 'IAdminDataMapper', useClass: AdminDataMapperImpl },
         { provide: 'IAdminStatisticsDataMapper', useClass: AdminStatisticsDataMapperImpl },
-        { provide: 'IScannerDataMapper', useClass: ScannerDataMapperImpl },
-        { provide: 'IWorkshopScansDataMapper', useClass: WorkshopDataMapperImpl },
-        { provide: 'ICheckoutObjectDataMapper', useClass: CheckoutObjectDataMapperImpl },
+        { provide: 'IAttendanceDataMapper', useClass: AttendanceDataMapperImpl },
         { provide: 'ICheckoutItemsDataMapper', useClass: CheckoutItemsDataMapperImpl },
-        { provide: 'IUrlDataMapper', useClass: UrlDataMapperImpl },
+        { provide: 'ICheckoutObjectDataMapper', useClass: CheckoutObjectDataMapperImpl },
+        { provide: 'IEventDataMapper', useClass: EventDataMapperImpl },
+        { provide: 'IExtraCreditDataMapper', useClass: ExtraCreditDataMapperImpl },
+        { provide: 'IHackathonDataMapper', useClass: HackathonDataMapperImpl },
+        { provide: 'ILocationDataMapper', useClass: LocationDataMapperImpl },
+        { provide: 'IPreRegisterDataMapper', useClass: PreRegisterDataMapperImpl },
         { provide: 'IProjectDataMapper', useClass: ProjectDataMapperImpl },
+        { provide: 'IRegisterDataMapper', useClass: RegisterDataMapperImpl },
+        { provide: 'IRsvpDataMapper', useClass: RsvpDataMapperImpl },
+        { provide: 'IScannerDataMapper', useClass: ScannerDataMapperImpl },
         { provide: 'IScoresDataMapper', useClass: ScoreDataMapperImpl },
-
+        { provide: 'IUpdateDataMapper', useClass: UpdateDataMapperImpl },
+        { provide: 'IUrlDataMapper', useClass: UrlDataMapperImpl },
+        { provide: 'IWorkshopScansDataMapper', useClass: WorkshopDataMapperImpl },
+        
         // Interfaces
         { provide: 'IAcl', useClass: RBAC },
         { provide: 'IAuthService', useClass: FirebaseAuthService },
-        { provide: 'IScannerAuthService', useClass: ApikeyAuthService },
-        { provide: 'IConnectionFactory', useClass: SqlConnectionFactory },
-        { provide: 'IRtdbFactory', useClass: RtdbConnectionFactory },
-        { provide: 'IPushNotifService', useClass: OnesignalService },
         { provide: 'ICacheService', useClass: MemCacheServiceImpl },
+        { provide: 'IConnectionFactory', useClass: SqlConnectionFactory },
         { provide: 'IEmailService', useClass: SendgridService },
+        { provide: 'IPushNotifService', useClass: OnesignalService },
+        { provide: 'IRtdbFactory', useClass: RtdbConnectionFactory },
+        { provide: 'IScannerAuthService', useClass: ApikeyAuthService },
         { provide: 'IStorageService', useClass: GoogleStorageService },
 
         // Classes
+        { provide: 'BunyanLogger', useClass: Logger },
         { provide: 'FirebaseService', useValue: FirebaseService.instance },
         { provide: 'RateLimiterService', useClass: RateLimiterService },
         { provide: 'MysqlUow', useClass: MysqlUow },
         { provide: 'RtdbUow', useClass: RtdbUow },
-        { provide: 'BunyanLogger', useClass: Logger },
       ],
     );
   }

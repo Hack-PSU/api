a0407a21ba9b538790db2503da1dfb85ac247966
jdurrell <john.m.durrell@gmail.com>
2022-07-24 15:06:40 -0400

    Created data mapper anad routes for organizers.

---
+++ src/models/admin/organizer.ts
@@ -0,0 +1,34 @@
+import BaseObject from "../../models/BaseObject";
+
+export interface IOrganizerApiModel {
+  uid: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  permission?: number;
+}
+
+export class Organizer extends BaseObject {
+
+  uid: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  permission?: number;
+
+  constructor(data: IOrganizerApiModel) {
+    super();
+    this.uid = data.uid;
+    this.email = data.email;
+    this.firstname = data.firstname;
+    this.lastname = data.lastname;
+    this.permission = data.permission;
+  }
+
+  public get id(): any {
+    return this.uid;
+  }
+  protected get schema(): any {
+    return null;
+  }
+}

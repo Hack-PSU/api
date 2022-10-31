23a0c0808110dfb214b692573db8d6727f82fd76
jdurrell <john.m.durrell@gmail.com>
2022-10-31 10:46:30 -0400

    Updated websocket pushes to include admin app for events and hackathons.

--- src/services/communication/websocket-pusher.ts
+++ src/services/communication/websocket-pusher.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { Injectable } from "injection-js";
 
+/* Handles pushing updates to mobile and admin applications by making a request to the websocket to ping
+   the mobile and admin applications to re-request the relevant information 
+*/
 @Injectable()
 export class WebsocketPusher {
 
@@ -10,15 +13,28 @@ export class WebsocketPusher {
   static HACKATHONS = 'hackathon';
   static EXTRA_CREDIT = 'extra-credit'
   static UPDATE_ROUTE_URL = 'https://ws.hackpsu.org/update/';
+
+  // ping constants
+  static MOBILE = 'MOBILE';
+  static ADMIN = 'ADMIN';
   
   constructor() {
 
   }
 
-  public async sendUpdateRequest(endpoint: string, idtoken: string) {
+  public async sendUpdateRequest(endpoint: string, pingTargets: string | string[], idtoken: string) {
     const route = WebsocketPusher.UPDATE_ROUTE_URL.concat(endpoint);
-    const body = { to: 'MOBILE' };
     const headers = { headers: { idtoken: idtoken }};
+    if (Array.isArray(pingTargets)) {
+      pingTargets.forEach(async element => {
+        this.doRequestIgnoringError(route, { to: element }, headers);
+      });
+    } else {
+      this.doRequestIgnoringError(route, { to: pingTargets }, headers);
+    }
+  }
+
+  public async doRequestIgnoringError(route: string, body: { to: string }, headers: { headers: { idtoken: string }}) {;
     try {
       axios.post(route, body, headers);
     } catch (error) {
